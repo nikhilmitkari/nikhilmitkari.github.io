cb22a6fddc6ce1c45e3f105e3377a4e4a75497ce
Nikhil Mitkari <66264406+nikhilmitkari@users.noreply.github.com>
2024-10-09 22:16:53 +0530

    adding scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    hideElementsAfterDelay('projects', 10000); // 10000 milliseconds = 10 seconds
+});
+
+// Function to hide elements with the specified class using jQuery
+function hideElementsAfterDelay(className, delay) {
+    setTimeout(() => {
+        $('#' + className).fadeOut(1000); // Use jQuery's hide() method
+    }, delay);
+}
